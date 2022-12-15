9ce6bbed86823714f93e3821c382b88468f034d4
Michal Voců <michal.vocu@gmail.com>
2022-12-15 16:57:31 +0100

    refactor (cont)

--- scripts/isuk/SchemaAdapter.groovy
+++ scripts/isuk/SchemaAdapter.groovy
@@ -1,7 +1,11 @@
+import groovy.sql.GroovyResultSet
+import groovy.sql.Sql
+import org.forgerock.openicf.misc.scriptedcommon.ICFObjectBuilder
 import org.identityconnectors.framework.common.objects.AttributeInfoBuilder
 import org.identityconnectors.framework.common.objects.Name
 import org.identityconnectors.framework.common.objects.Uid
 
+import java.time.ZoneId
 import java.time.ZonedDateTime
 
 import static org.identityconnectors.framework.common.objects.AttributeInfo.Flags.MULTIVALUED
@@ -139,4 +143,122 @@ class SchemaAdapter {
                 "poid_nadrizeny"   : "poid_nadrizeny"
         ]
     }
+
+    def static getPersonFieldMap() {
+        return [
+                "__UID__"     : "cislo_osoby",
+                "__NAME__"    : "cuni_unique_id",
+                "jmeno"		: "jmeno",
+                "prijmeni"	: "prijmeni",
+                "rodne_cislo"	: "rodne_cislo",
+                "datum_narozeni" : "datum_narozeni",
+                "stat"		: "st",
+                "pohlavi"	: "pohlavi",
+                "preferred_language" : "preferredlanguage"
+                /*
+                 preferred_language String.class
+                 handicap String.class, MULTIVALUED
+                 mail String.class, MULTIVALUED
+                 phone String.class, MULTIVALUED
+                 mobile String.class, MULTIVALUED
+                 identifikace String.class
+                 adresa_stat String.class
+                 adresa_mesto String.class
+                 adresa_ulice String.class
+                 adresa_cev String.class
+                 adresa_corg String.class
+                 adresa_psc String.class
+                 uid String.class
+         */
+        ]
+    }
+
+    def static getGroupFieldMap() {
+        return [
+                "__UID__"    	: "id",
+                "__NAME__"   	: "name"
+        ]
+    }
+
+
+    def static mapOrganizationToIcfObject(GroovyResultSet row, Sql sql) {
+        return ICFObjectBuilder.co {
+                    uid row.id_org.toString()
+                    id row.poid.toString()
+                    setObjectClass objectClass
+                    attribute 'r_id_org', row.id_org
+                    attribute 'fakulta', row.fakulta
+                    attribute 's_fakulta', row.fakulta.toString()
+                    attribute 'sidlo', row.sidlo
+                    attribute 'datum_od', ZonedDateTime.ofInstant(row.datum_od.toInstant(), ZoneId.systemDefault())
+                    attribute 'datum_do', ZonedDateTime.ofInstant(row.datum_do.toInstant(), ZoneId.systemDefault())
+                    attribute 'ic', row.ic
+                    attribute 'dic', row.dic
+                    attribute 'r_poid', row.poid
+                    attribute 'nazev', row.nazev
+                    attribute 'nazev_dlouhy', row.nazev_dlouhy
+                    attribute 'nazev_en', row.nazev_en
+                    attribute 'nazev_dlouhy_en', row.nazev_dlouhy_en
+                    attribute 'zkratka', row.zkratka
+                    attribute 'zkratka_dlouha', row.zkratka_dlouha
+                    attribute 'zkratka_en', row.zkratka_en
+                    attribute 'zkratka_dlouha_en', row.zkratka_dlouha_en
+                    attribute 'soucast', row.soucast
+                    attribute 's_soucast', row.soucast.toString()
+                    attribute 'kod_sims', row.kod_sims
+                    attribute 'cas_domena', row.cas_domena
+                    attribute 'cas_identifikace', row.cas_identifikace
+                    attribute 'id_org_nadrizeny', row.id_org_nadrizeny.split(',').collect { value -> new BigInteger(value) }
+                    attribute 'poid_nadrizeny', row.poid_nadrizeny.split(',').collect { value-> new BigInteger(value) }
+                    attribute 's_poid_nadrizeny', row.poid_nadrizeny.split(',')
+        }
+    }
+
+
+    def static mapPersonToIcfObject(GroovyResultSet row, Sql sql) {
+        return ICFObjectBuilder.co {
+                    uid row.cislo_osoby.toString()
+                    id row.cuni_unique_id.toString()
+                    setObjectClass objectClass
+                    attribute 'jmeno', row.jmeno
+                    attribute 'prijmeni', row.prijmeni
+                    attribute 'rodne_cislo', row.rodne_cislo
+                    attribute 'datum_narozeni', ZonedDateTime.ofInstant(row.datum_narozeni.toInstant(), ZoneId.systemDefault())
+                    attribute 'stat', row.st
+                    attribute 'pohlavi', row.pohlavi
+                    attribute 'preferred_language', row.preferredlanguage
+                    // handicap
+                    attribute 'handicap', sql.rows(["id" : row.cislo_osoby],
+                            "SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'cuniHandicap' AND cislo_osoby = :id"
+                    )*.hodnota
+
+                    // mail
+                    attribute 'mail', sql.rows(["id" : row.cislo_osoby],
+                            "SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'mail' AND cislo_osoby = :id"
+                    )*.hodnota
+                    // mail_o365
+                    attribute 'mail_o365', sql.rows(["id" : row.cislo_osoby],
+                            "SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'mail_o365' AND cislo_osoby = :id"
+                    )*.hodnota
+                    // phone
+                    attribute 'phone', sql.rows(["id" : row.cislo_osoby],
+                            "SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'telephoneNumber' AND cislo_osoby = :id"
+                    )*.hodnota
+                    // mobile
+                    attribute 'mobile', sql.rows(["id" : row.cislo_osoby],
+                            "SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'mobile' AND cislo_osoby = :id"
+                    )*.hodnota
+                    /*
+                    identifikace String.class
+                    adresa_stat String.class
+                    adresa_mesto String.class
+                    adresa_ulice String.class
+                    adresa_cev String.class
+                    adresa_corg String.class
+                    adresa_psc String.class
+                    uid String.class
+                     */
+        }
+    }
+
 }
--- scripts/isuk/SearchScript.groovy
+++ scripts/isuk/SearchScript.groovy
@@ -2,11 +2,9 @@ import groovy.sql.Sql
 import groovy.text.SimpleTemplateEngine
 
 import org.forgerock.openicf.connectors.scriptedsql.ScriptedSQLConfiguration
-import org.forgerock.openicf.misc.scriptedcommon.ICFObjectBuilder
 import org.forgerock.openicf.misc.scriptedcommon.OperationType
 import org.identityconnectors.common.StringUtil
 import org.identityconnectors.common.logging.Log
-import org.identityconnectors.framework.common.exceptions.ConnectorException
 import org.identityconnectors.framework.common.objects.AttributeUtil
 import org.identityconnectors.framework.common.objects.ObjectClass
 import org.identityconnectors.framework.common.objects.OperationOptions
@@ -15,10 +13,7 @@ import org.identityconnectors.framework.common.objects.SearchResult
 import org.identityconnectors.framework.common.objects.Uid
 import org.identityconnectors.framework.common.objects.filter.EqualsFilter
 import org.identityconnectors.framework.common.objects.filter.Filter
-import org.identityconnectors.framework.spi.SearchResultsHandler
 import java.sql.Connection
-import java.time.ZonedDateTime
-import java.time.ZoneId
 
 def log = log as Log
 def operation = operation as OperationType
@@ -57,65 +52,9 @@ log.warn("Page size " + pageSize + ", page offset " + pageOffset +  ", page cook
 //and is used to translate fields that might exist in the query object from the ICF identifier
 //back to the real property name.
 def fieldMap = [
-        ( BaseScript.ORGANIZATION_NAME ): SchemaAdapter.getOrganizationFieldMap(),
-		/* [
-                "__UID__"    : "id_org",
-                "__NAME__"   : "poid",
-                "r_id_org"      : "id_org",
-                "fakulta"       : "fakulta",
-                "sidlo"         : "sidlo",
-                "datum_od"      : "datum_od",
-                "datum_do"      : "datum_do",
-                "ic"            : "ic",
-                "dic"           : "dic",
-                "r_poid"        : "poid",
-                "nazev"         : "nazev",
-                "nazev_dlouhy"  : "nazev_dlouhy",
-                "nazev_en"      : "nazev_en",
-                "nazev_dlouhy_en" : "nazev_dlouhy_en",
-                "zkratka"       : "zkratka",
-                "zkratka_dlouha": "zkratka_dlouha",
-                "zkratka_en"    : "zkratka_en",
-                "zkratka_dlouha_en" : "zkratka_dlouha_en",
-                "soucast"       : "soucast",
-                "kod_sims"      : "kod_sims",
-                "cas_domena"    : "cas_domena",
-                "cas_identifikace" : "cas_identifikace",
-                "id_org_nadrizeny" : "id_org_nadrizeny",
-				"poid_nadrizeny"   : "poid_nadrizeny"
-        ], */
-
-	( BaseScript.PERSON_NAME ): [
-                "__UID__"     : "cislo_osoby",
-                "__NAME__"    : "cuni_unique_id",
-				"jmeno"		: "jmeno",
-				"prijmeni"	: "prijmeni",
-				"rodne_cislo"	: "rodne_cislo",
-				"datum_narozeni" : "datum_narozeni",
-				"stat"		: "st",
-				"pohlavi"	: "pohlavi",
-				"preferred_language" : "preferredlanguage"
-		/*
-		 preferred_language String.class
-		 handicap String.class, MULTIVALUED
-		 mail String.class, MULTIVALUED
-		 phone String.class, MULTIVALUED
-		 mobile String.class, MULTIVALUED
-		 identifikace String.class
-		 adresa_stat String.class
-		 adresa_mesto String.class
-		 adresa_ulice String.class
-		 adresa_cev String.class
-		 adresa_corg String.class
-		 adresa_psc String.class
-		 uid String.class
- */
-        ],
-	
-        ( BaseScript.GROUP_NAME )  : [
-                "__UID__"    	: "id",
-                "__NAME__"   	: "name"
-        ]
+	( BaseScript.ORGANIZATION_NAME ): SchemaAdapter.getOrganizationFieldMap(),
+	( BaseScript.PERSON_NAME ): SchemaAdapter.getPersonFieldMap(),
+	( BaseScript.GROUP_NAME )  : SchemaAdapter.getGroupFieldMap()
 ]
 
 def attrs = fieldMap[objectClass.objectClassValue].collect([] as HashSet){ entry -> entry.value }
@@ -188,100 +127,28 @@ log.warn("Found {0} total records", total)
 int lastRowNum = 0
 def rowCount = 0
 sql.eachRow((Map) whereParams, (String) sqlquery, { row ->
-
-    def connectorObject = ICFObjectBuilder.co {
         switch (objectClass) {
 
             case BaseScript.PERSON:
-                uid row.cislo_osoby.toString()
-                id row.cuni_unique_id.toString()
-				setObjectClass objectClass
-				attribute 'jmeno', row.jmeno
-				attribute 'prijmeni', row.prijmeni
-				attribute 'rodne_cislo', row.rodne_cislo
-				attribute 'datum_narozeni', ZonedDateTime.ofInstant(row.datum_narozeni.toInstant(), ZoneId.systemDefault())
-				attribute 'stat', row.st
-				attribute 'pohlavi', row.pohlavi
-				attribute 'preferred_language', row.preferredlanguage
-				// handicap
-				attribute 'handicap', sql.rows(["id" : row.cislo_osoby],
-					"SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'cuniHandicap' AND cislo_osoby = :id"
-					)*.hodnota
-			
-				// mail
-				attribute 'mail', sql.rows(["id" : row.cislo_osoby],
-					"SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'mail' AND cislo_osoby = :id"
-					)*.hodnota
-				// mail_o365
-				attribute 'mail_o365', sql.rows(["id" : row.cislo_osoby],
-					"SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'mail_o365' AND cislo_osoby = :id"
-					)*.hodnota
-				// phone
-				attribute 'phone', sql.rows(["id" : row.cislo_osoby],
-					"SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'telephoneNumber' AND cislo_osoby = :id"
-					)*.hodnota
-				// mobile
-				attribute 'mobile', sql.rows(["id" : row.cislo_osoby],
-					"SELECT DISTINCT hodnota FROM skunk_cas.ldap_ruzne WHERE nazev = 'mobile' AND cislo_osoby = :id"
-					)*.hodnota
-		/*		
-		identifikace String.class
-		adresa_stat String.class
-		adresa_mesto String.class
-		adresa_ulice String.class
-		adresa_cev String.class
-		adresa_corg String.class
-		adresa_psc String.class
-		uid String.class
-*/
+				SchemaAdapter.mapPersonToIcfObject(row, sql)
                 break;
 /*
             case ObjectClass.GROUP:
-                uid row.id as String
-                id row.name
                 break;
 */
             case BaseScript.ORGANIZATION:
-	        	uid row.id_org.toString()
-       	     	id row.poid.toString()
-				setObjectClass objectClass
-            	attribute 'r_id_org', row.id_org
-            	attribute 'fakulta', row.fakulta
-				attribute 's_fakulta', row.fakulta.toString()
-            	attribute 'sidlo', row.sidlo
-            	attribute 'datum_od', ZonedDateTime.ofInstant(row.datum_od.toInstant(), ZoneId.systemDefault())
-            	attribute 'datum_do', ZonedDateTime.ofInstant(row.datum_do.toInstant(), ZoneId.systemDefault())
-            	attribute 'ic', row.ic
-            	attribute 'dic', row.dic
-            	attribute 'r_poid', row.poid
-            	attribute 'nazev', row.nazev
-            	attribute 'nazev_dlouhy', row.nazev_dlouhy
-            	attribute 'nazev_en', row.nazev_en
-            	attribute 'nazev_dlouhy_en', row.nazev_dlouhy_en
-            	attribute 'zkratka', row.zkratka
-            	attribute 'zkratka_dlouha', row.zkratka_dlouha
-            	attribute 'zkratka_en', row.zkratka_en
-            	attribute 'zkratka_dlouha_en', row.zkratka_dlouha_en
-            	attribute 'soucast', row.soucast
-				attribute 's_soucast', row.soucast.toString()
-            	attribute 'kod_sims', row.kod_sims
-            	attribute 'cas_domena', row.cas_domena
-            	attribute 'cas_identifikace', row.cas_identifikace
-            	attribute 'id_org_nadrizeny', row.id_org_nadrizeny.split(',').collect { value -> new BigInteger(value) }
-				attribute 'poid_nadrizeny', row.poid_nadrizeny.split(',').collect { value-> new BigInteger(value) }
-				attribute 's_poid_nadrizeny', row.poid_nadrizeny.split(',')
+				SchemaAdapter.mapOrganizationToIcfObject(row, sql)
                 break;
 
             default:
                 throw new UnsupportedOperationException(operation.name() + " operation of type:" +
                         objectClass.objectClassValue + " is not supported.")
         }
-    }
 
-    lastRowNum = (int)row.radek
-    rowCount++
+    	lastRowNum = (int)row.radek
+    	rowCount++
     
-    handler.handle connectorObject
+	    handler.handle connectorObject
 })
 
 
